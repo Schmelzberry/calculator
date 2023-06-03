bc8cf6bf924b9b589086768515ac6394aed0f4a0
Schmelzberry <jelsberry96@gmail.com>
2023-06-02 21:10:34 -0700

    calculator gathers form data

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+// Business logic
+function add(number1, number2) {
+    return number1 + number2;
+}
+
+function subtract(number1, number2) {
+    return number1 - number2;
+}
+
+function multiply(number1, number2) {
+    return number1 * number2;
+}
+function divide(number1, number2) {
+    return number1 / number2;
+}
+
+
+
+
+
+// UI logic
+
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+window.alert(add(number1, number2));
+window.alert(subtract(number1, number2));
+
+
+window.alert(multiply(number1, number2));
+window.alert(divide(number1, number2));
+100
