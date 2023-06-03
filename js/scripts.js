41ce393a9faa0b2b03bd9a6da6554a9d6c844540
Schmelzberry <jelsberry96@gmail.com>
2023-06-02 21:33:30 -0700

    revising js functions

--- js/scripts.js
+++ js/scripts.js
@@ -24,8 +24,6 @@ const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 window.alert(add(number1, number2));
 window.alert(subtract(number1, number2));
-
-
 window.alert(multiply(number1, number2));
 window.alert(divide(number1, number2));
-100
+
