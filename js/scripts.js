f93735e71900e2796e7e053ef5db62a87fa7ad1b
Schmelzberry <jelsberry96@gmail.com>
2023-06-10 20:12:33 -0700

    performs all 4 math basic math functions

--- js/scripts.js
+++ js/scripts.js
@@ -16,18 +16,57 @@ class Calculator {
     delete() {
 
     }
-
+    // 
+    // originally, apendNumber()'s 2nd line was 'this.currentOperand = number' and it returned only a single number at a time. Now, it allows for a full string of numbers to be entered. It also only allows for one decimal point per number. **Note** the return statement at end of line 22 tells the if statement to essentially exit the function if the number includes a decimal and the user tries to enter another decimal.
     appendNumber(number) {
-        this.currentOperand = number;
+        if (number === "." && this.currentOperand.includes('.')) return
+        this.currentOperand = this.currentOperand.toString() + number.toString()
+     }
+
+     chooseOperation(operation) {
+        if (this.currentOperand === '') return
+        if (this.previousOperand !== '') {
+            this.compute()
+        }
+        this.operation = operation
+        this.previousOperand = this.currentOperand
+        this.currentOperand = ''
+
      }
 
 
     compute() {
+        let computation
+        const prev = parseFloat(this.previousOperand)
+        const current = parseFloat(this.currentOperand)
+        if (isNaN(prev) || isNaN(current)) return
+        switch (this.operation) {
+        case '+':
+        computation = prev + current
+        break
+        case '-':
+        computation = prev - current
+        break
+        case '*':
+        computation = prev * current
+        break
+        case '÷':
+        computation = prev / current
+        break
+        default:
+            return
+
+        }
+        this.currentOperand = computation
+        this.operation = undefined
+        this.previousOperand = ''
 
     }
 
     updateDisplay() {
         this.currentOperandTextElement.innerText = this.currentOperand
+        this.previousOperandTextElement.innerText = this.previousOperand
+        
 
     }
 }
@@ -49,10 +88,25 @@ numberButtons.forEach(button => {
         calculator.updateDisplay()
     })
 })
-    
+   
+operationButtons.forEach(button => {
+    button.addEventListener('click', ()=> {
+        calculator.chooseOperation(button.innerText)
+        calculator.updateDisplay()
+    })
+})
 
+equalsButton.addEventListener('click', button => {
+    calculator.compute()
+    calculator.updateDisplay()
 
+})
 
+allClearButton.addEventListener('click', button => {
+    calculator.clear()
+    calculator.updateDisplay()
+
+})
 
 
 
